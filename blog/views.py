6bea9b0a5a1f34b912c8dc4305ee39e0e7311323
MileySJ <ohmiley007@gmail.com>
2022-06-28 13:45:55 +0100

    order by date

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Blog
 
 def bloggy(request):
-    blogs = Blog.objects.order_by(-date)
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/blog.html', {'blogs':blogs})
