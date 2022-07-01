97692f2ed2b9c69f3906efa5db8a5449b91d3c3e
MileySJ <ohmiley007@gmail.com>
2022-07-01 13:48:29 +0100

    add link to blog in home page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Blog
 
-def bloggy(request):
+def blog(request):
     blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/blog.html', {'blogs':blogs})
