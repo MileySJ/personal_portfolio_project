f386c2c376c5edde82bd9949f39c49a7726f0d6a
MileySJ <ohmiley007@gmail.com>
2022-07-04 18:48:34 +0100

    add path for blog id

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from .models import Blog
 def blog(request):
     blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/blog.html', {'blogs':blogs})
+
+def details(request, blog_id):
+    return render(request, 'blog/detail.html', {'id':blog_id})
